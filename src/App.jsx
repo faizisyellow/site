a78ec1593d2ea738fdd098848da0ef94c9bd022d
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-08-22 02:55:53 +0700

    configs: install tailwind, fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-sans font-normal text-2xl">Geist Regular (400)</h1>
+
+      <h1 className="font-sans font-medium text-2xl">Geist Medium (500)</h1>
+
+      <h1 className="font-sans font-bold text-2xl">Geist Bold (700)</h1>
+    </>
+  );
+}
+
+export default App;
