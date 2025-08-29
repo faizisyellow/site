554d50e14c0deb6ec34461b8dddf979b5bd60632
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-08-29 17:37:55 +0700

    content: update ABOUT

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <TitleSection title="About Me" />
 
       <p className="text-brand text-lg text-pretty leading-relaxed min-h-[4.875rem]">
-       Hi! My name is Faissal Maulana. I’m a Backend developer passionate about building robust and scalable web services. I follow best practices and design patterns to ensure reliability. I’m also interested in terminals and CLI tools, and I like creating CLI tools to reduce repetitive tasks so I can focus on solving problems, implementing business logic, and speeding up development.
+       Hi! My name is Faissal Maulana. I’m a Backend developer passionate about building robust and scalable web services. I follow best practices and design patterns to ensure reliability. I’m also interested in terminals and terminal tools, and I like creating CLI tools to reduce repetitive tasks so I can focus on solving problems, implementing business logic, and speeding up development.
       </p>
     </div>
   );
